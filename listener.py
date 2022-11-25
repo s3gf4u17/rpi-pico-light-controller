a2dd8b3de8c9371b9b4dda44e36aea758a1603d9
szkolakowski <szymon.kolakowski@gmail.com>
2022-11-25 21:18:43 +0000

    write 16 char message to blink

--- listener.py
+++ listener.py
@@ -4,6 +4,13 @@ subprocess.run(['sudo','chmod','a+wr','/dev/ttyACM0'])
 
 ser=serial.Serial('/dev/ttyACM0',timeout=2)
 
+# while True:
+#     content=ser.readline()
+#     print(content)
+
+string = '0123456789012345'
+ser.write(string.encode())
 while True:
+    ser.write(string.encode())
     content=ser.readline()
     print(content)
