7fff325e6f8401dfedf688f9b883fa2224941276
szkolakowski <szymon.kolakowski@gmail.com>
2022-11-25 17:08:35 +0000

    update printf (with io init to connect as tty/ACM)

--- blink/blink.c
+++ blink/blink.c
@@ -5,19 +5,21 @@
  */
 
 #include "pico/stdlib.h"
+#include "limits.h"
+#include <stdio.h>
 
 int main() {
-#ifndef PICO_DEFAULT_LED_PIN
-#warning blink example requires a board with a regular LED
-#else
+    stdio_init_all();
+    int counter = 0;
     const uint LED_PIN = PICO_DEFAULT_LED_PIN;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
     while (true) {
+        counter = (counter == INT_MAX) ? 0 : counter++;
+        printf("%i Hello World!\n",counter);
         gpio_put(LED_PIN, 1);
         sleep_ms(250);
         gpio_put(LED_PIN, 0);
         sleep_ms(250);
     }
-#endif
 }
