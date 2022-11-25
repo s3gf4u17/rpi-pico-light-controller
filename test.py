da962617f135c486eb3474c475eb00172cb370ed
szkolakowski <szymon.kolakowski@gmail.com>
2022-11-25 22:58:40 +0000

    html from external template

--- test.py
+++ test.py
@@ -6,41 +6,7 @@ import serial, subprocess
 app = FastAPI()
 subprocess.run(['sudo','chmod','a+wr','/dev/ttyACM0'])
 ser=serial.Serial('/dev/ttyACM0',timeout=2)
-
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var ws = new WebSocket("ws://localhost:8000/ws");
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-"""
-
+with open("template.html","r") as f: html=f.read()
 
 @app.get("/")
 async def get():
